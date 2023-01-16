fc224d82acb0d980230e83e8d52ee13bb68d62c8
uttam kumar patra <dev.uttampatra@gmail.com>
2023-01-16 22:29:20 +0530

    header done

---
+++ header.js
@@ -0,0 +1,16 @@
+const header = document.querySelector("header");
+header.innerHTML = `
+<nav>
+<h2>Digital <span>Hub</span></h2>
+<ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About Us</a></li>
+    <li><a href="#">Services</a></li>
+    <li><a href="#">Contect</a></li>
+</ul>
+<div class="log">
+    <a href="#">Login</a>
+    <a class="signup" href="#">Sign Up</a>
+</div>
+</nav>
+`;
