69934c293fd775608f2ce85d73014cb680d87d24
uttam kumar patra <dev.uttampatra@gmail.com>
2023-01-17 13:13:34 +0530

    first responsive design done

--- header.js
+++ header.js
@@ -2,6 +2,7 @@ const header = document.querySelector("header");
 header.innerHTML = `
 <nav>
 <h2>Digital <span>Hub</span></h2>
+<i class="bi bi-list"></i>
 <ul>
     <li><a href="#home">Home</a></li>
     <li><a href="#about">About Us</a></li>
