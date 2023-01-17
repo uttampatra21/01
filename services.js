491883326ab523d81279e7b2251fad4ab9874c92
uttam kumar patra <dev.uttampatra@gmail.com>
2023-01-17 15:37:24 +0530

    our services

---
+++ services.js
@@ -0,0 +1,43 @@
+const services = document.querySelector("#services");
+services.innerHTML = `
+<h1>OUR SERVICES</h1>
+<p></p>
+<div class="service_box">
+    <div class="box_one">
+        <div class="box_one_text">
+            <h4>WEBSITE DEVELOPMENT</h3>
+            <p>We help build a clean, creative and professional website store to connect to your clients.</p>
+        </div>
+        <div class="box_two">
+            <img src="https://cdn.shopify.com/s/files/1/0589/7168/5055/files/Web_Development_grande.png?v=1650430549">
+        </div>
+    </div>
+    <div class="box_one">
+        <div class="box_two">
+            <img src="https://cdn.shopify.com/s/files/1/0589/7168/5055/files/Digital_Marketing_grande.png?v=1650430536">
+        </div>
+        <div class="box_two box_one_text">
+            <h4>DIGITAL MARKETING</h4>
+            <p>We offer delightful components of marketing & a cost-effective way to grow businesses and increase sales with targeted audience reach.</p>
+        </div>
+    </div>
+    <div class="box_one">
+        <div class="box_one_text">
+            <h4>DIGITAL MARKETING</h4>
+            <p>Cost-effective way to grow businesses and increase sales with targeted audience reach.</p>
+        </div>
+        <div class="box_two">
+            <img src="https://cdn.shopify.com/s/files/1/0589/7168/5055/files/Ecommerce_grande.png?v=1650430524" >
+        </div>
+    </div>
+    <div class="box_one">
+        <div class="box_two">
+            <img src="https://cdn.shopify.com/s/files/1/0589/7168/5055/files/Others_grande.png?v=1650430575">
+        </div>
+        <div class="box_two box_one_text">
+            <h4>OTHERS</h4>
+            <p>We also offer other services that include photoshoot, video creation and Graphic designing.</p>
+        </div>
+    </div>
+</div>
+`;
